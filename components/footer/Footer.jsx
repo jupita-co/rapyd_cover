8c1b2f965987b90ed9905d7b4812af0e25739fe9
Ayo <a@MacBook-Pro.local>
2023-02-14 22:39:25 -0700

    update

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = () => {
           <div className={style.left}>
             <Image src={Logo} alt="logo" />
           </div>
+
           <div className={style.right}>
             <div className={style.icons}>
               <Image src={Tw} alt="twitter" />
@@ -43,6 +44,36 @@ const Footer = () => {
                 <p>Privacy Policy</p>
                 <p>Terms of service</p>
               </div>
+            </div>
+          </div>
+
+          <div className={style.rightM}>
+            <div className={style.linksM}>
+              <div className={style.linksTopM}>
+                <Link href="" />
+                <p>About us</p>
+
+                <Link href="">
+                  <p>Services</p>
+                </Link>
+                <Link href="">
+                  {" "}
+                  <p>Locations</p>
+                </Link>
+                <Link href="">
+                  <p>FAQs</p>
+                </Link>
+              </div>
+            </div>
+            <div className={style.iconsM}>
+              <Image src={Tw} alt="twitter" />
+              <Image src={In} alt="linkedIn" />
+              <Image src={Fb} alt="facebook" />
+              <Image src={Ig} alt="instagram" />
+            </div>
+            <div className={style.linksBodyM}>
+              <p>Privacy Policy</p>
+              <p>Terms of service</p>
             </div>
           </div>
         </div>
